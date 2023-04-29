7d758115b803ea0833613d84163af041e5639836
Adijair Araujo <69972680+Adijair@users.noreply.github.com>
2023-04-29 13:07:55 -0300

    Delete Projeto4.py

--- Projeto4.py
+++
@@ -1,8 +0,0 @@
-#faça um progama em que diga se o número inteiro digitado na tela é par ou impar
-
-numero = int(input('Me diga um numero: '))
-resutado = numero % 2
-if resutado == 0:
-    print('O numero {} é par '. format(numero))
-else:
-    print('O numero {} é inpar '. format(numero))
