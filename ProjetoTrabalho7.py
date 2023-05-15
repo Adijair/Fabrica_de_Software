2b48db96416e15d52c11d0ee3d71a1f67432d7d0
Adijair Araujo <69972680+Adijair@users.noreply.github.com>
2023-05-15 20:43:11 -0300

    Delete ProjetoTrabalho7.py

--- ProjetoTrabalho7.py
+++
@@ -1,9 +0,0 @@
-sexo = ""
-while sexo != 'M' and sexo != 'F':
-    sexo = input("Digite o sexo (M ou F): ")
-    sexo = sexo.upper()
-
-    if sexo != 'M' and sexo != 'F':
-        print("Valor incorreto. Digite novamente.")
-
-print("Sexo válido:", sexo)
